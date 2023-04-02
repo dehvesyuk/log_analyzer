e9e84ec866ec356ab1d0c58bc76dae1482f62616
dmitry-khvesyuk <d.khvesyuk@incrdbl.me>
2023-04-02 18:07:58 +0300

    working with log dir

--- helpers.py
+++ helpers.py
@@ -1,18 +1,33 @@
 import gzip
 import os
+from datetime import datetime
+
+LOG_FILENAME_TEMPLATE = "nginx-access-ui.log"
+REPORT_FILENAME_TEMPLATE = "report-"
 
 
 def log_reader(filename: str):
     name, ext = os.path.splitext(filename)
-    open_method = gzip.open if ext == '.gz' else open
-    with open_method(filename, 'rb') as line:
+    open_method = gzip.open if ext == ".gz" else open
+    with open_method(filename, "rb") as line:
         yield line.readline()
 
 
-def get_last_log_filename(log_dir: str):
-    log_lst = os.listdir(log_dir)
+def get_last_log_filename(log_dir: str) -> str:
+    dir_files = os.listdir(log_dir)
+    log_list = [x for x in dir_files if LOG_FILENAME_TEMPLATE in x]
+    log_list.sort(reverse=True, key=lambda x: x.split('.')[1])
+    return log_list[0]
 
 
-def get_last_parsed_log_date() -> str:
-    # в папке ./report найти дату последнего сформированного отчета
-    pass
+def get_last_report_filename(report_dir: str) -> str:
+    dir_files = os.listdir(report_dir)
+    report_list = [x for x in dir_files if REPORT_FILENAME_TEMPLATE in x]
+    report_list.sort(reverse=True)
+    return report_list[0]
+
+
+def is_log_and_report_date_equal(log: str, report: str) -> bool:
+    log_datetime = datetime.strptime(log.split('.')[1], "log-%Y%m%d")
+    report_datetime = datetime.strptime(report, "report-%Y.%m.%d.html")
+    return log_datetime == report_datetime
--- log_analyzer.py
+++ log_analyzer.py
@@ -1,7 +1,6 @@
 import os.path
 
-from helpers import log_reader, parse_filename
-
+from helpers import get_last_log_filename, get_last_report_filename, is_log_and_report_date_equal, log_reader
 
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
@@ -20,7 +19,14 @@ def main():
     log_dir = config["LOG_DIR"]
     if not os.path.exists(report_dir):
         os.makedirs(report_dir)
-    log_file = parse_filename(log_dir)
+
+    last_log = get_last_log_filename(log_dir)
+    last_report = get_last_report_filename(report_dir)
+    if is_log_and_report_date_equal(last_log, last_report):
+        return
+
+    for line in log_reader(last_log):
+        pass
 
 
 if __name__ == "__main__":
